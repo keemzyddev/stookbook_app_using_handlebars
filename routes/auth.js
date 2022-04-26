5b0c6a57675e4d375da12e1236f8cc1708a3e02e
Hakeem <hakeemdurogbola@gmail.com>
2022-04-26 11:39:09 +0100

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+//auth with Google
+//Get /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'], prompt: 'select_account', }))
+
+// google auth callback
+// Get /auth/google/callback
+router.get('/google/callback',
+ passport.authenticate('google', {failureRedirect: '/', }),
+ (req, res) =>{
+    res.redirect('/dashboard')
+} 
+)
+//logout user
+///auth/logout
+router.get('/logout', (req, res)=>{
+    req.logout()
+    res.redirect('/')
+})
+
+module.exports = router
